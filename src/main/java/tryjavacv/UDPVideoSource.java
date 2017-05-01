fdd1900204257a9ce2b78689ccb6995f7978191b
Shalom Crown <shalomc@airobotics.co.il>
2017-05-01 13:18:04 +0300

    Added windows support (standard laptops)

--- src/main/java/tryjavacv/UDPVideoSource.java
+++ src/main/java/tryjavacv/UDPVideoSource.java
@@ -5,7 +5,12 @@ import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.Executor;
 
+import com.sun.jna.Platform;
+
 public class UDPVideoSource {
+
+	public static final long TIME_BETWEEN_PROCESSES = 150;
+
 
 	public UDPVideoSource() {
 		new Thread(new Runnable() {
@@ -14,12 +19,20 @@ public class UDPVideoSource {
 			public void run() {
 				try {
 					while (true) {
-						CommandLine cmdLine;
+						CommandLine cmdLine = null;
 						ExecuteWatchdog watchdog;
 						Executor executor;
 						try {
 							System.out.println("Start standard scale FFMPEG");
-							cmdLine = CommandLine.parse("ffmpeg -i /dev/video0 -f mpegts udp://0.0.0.0:4443");
+
+							if (Platform.isLinux()) {
+								cmdLine = CommandLine.parse("ffmpeg -i /dev/video0 -f mpegts udp://0.0.0.0:4443");
+							} else if (Platform.isWindows()) {
+								cmdLine = CommandLine.parse("ffmpeg -f dshow -i video=\"Integrated Camera\" -f mpegts udp://0.0.0.0:4443");
+
+							} else {
+								throw new Exception("Unknown platform");
+							}
 
 							watchdog = new ExecuteWatchdog(10 * 1000);
 							executor = new DefaultExecutor();
@@ -29,10 +42,18 @@ public class UDPVideoSource {
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
+
+						Thread.sleep(TIME_BETWEEN_PROCESSES);
 
 						try {
 							System.out.println("Start altered scale FFMPEG");
-							cmdLine = CommandLine.parse("ffmpeg -i /dev/video0 -vf scale=240:200 -f mpegts udp://0.0.0.0:4443");
+							if (Platform.isLinux()) {
+								cmdLine = CommandLine.parse("ffmpeg -i /dev/video0 -vf scale=240:200 -f mpegts udp://0.0.0.0:4443");
+							} else if (Platform.isWindows()) {
+								cmdLine = CommandLine.parse("ffmpeg -f dshow -i video=\"Integrated Camera\" -vf scale=240:200 -f mpegts udp://0.0.0.0:4443");
+							} else {
+								throw new Exception("Unknown platform");
+							}
 
 							watchdog = new ExecuteWatchdog(10 * 1000);
 							executor = new DefaultExecutor();
@@ -42,6 +63,8 @@ public class UDPVideoSource {
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
+
+						Thread.sleep(TIME_BETWEEN_PROCESSES);
 					}
 
 				} catch (Exception e1) {
